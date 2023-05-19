626981ac2c8379afbd8438ceb3f55222d6592b35
Claudio Manoel Goncalves <cmgoncalv70@gmail.com>
2023-05-19 09:08:45 -0300

    Lesson 303 entity Order and dates with instant and standard ISO 8601

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,11 +1,14 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +24,10 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	//cria a associação com o usuario e instancia a collection
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();	
 	
 	//as we are using framework, it's required to use empty constructor
 	public User() {		
@@ -74,6 +81,10 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	@Override
 	public int hashCode() {
@@ -100,5 +111,4 @@ public class User implements Serializable {
 		return true;
 	}
 
-	
 }
