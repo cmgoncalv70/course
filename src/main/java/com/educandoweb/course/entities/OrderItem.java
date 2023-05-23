cd28b89d5807b7ce35c1158a35d000bfe66c1ec6
Claudio Manoel Goncalves <cmgoncalv70@gmail.com>
2023-05-23 18:39:39 -0300

    Lesson 311 Many to Many relationship between Product and OrderItem

--- src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -48,12 +48,7 @@ public class OrderItem implements Serializable {
 	public void setProduct(Product product) {
 		id.setProduct(product);
 	}
-	
-	
-	
-	
-	
-	
+		
 	public Integer getQuantity() {
 		return quantity;
 	}
